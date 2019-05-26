ec764151bd38a5ba8d4b320e336b7854268ee809
Harmony <hkunzler@gmail..com>
2019-05-25 18:59:33 -0600

    setup graphql server

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const {graphqlExpress, graphiqlExpress} = require('graphql-server-express')
+const {makeExecutableSchema} = require('graphql-tools')
+
+const typeDefs = `
+  type Query {
+    hello: String
+  }
+`
+
+const resolvers = {
+  Query: {
+    hello: () => ' Hello World'
+  }
+}
+
+const schema = makeExecutableSchema({ typeDefs, resolvers})
+
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+app.use('/graphql', bodyParser.json(), graphqlExpress({
+  schema
+}))
+app.use('/graphiql', graphiqlExpress({
+  endpointURL: '/graphql'
+}))
+app.listen(PORT,
+() => console.log(`Server is running on port ${PORT}`))
